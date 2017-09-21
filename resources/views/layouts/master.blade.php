d5bc0a9a10f8e0561d0ce8dbf2fe8203e142e1d4
thuytiensp <thuytiensp@gmail.com>
2017-09-21 17:26:05 +0700

    Add some CSS and using Facade to link to css file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,8 @@
         <title>Laravel</title>     
         <!-- Latest compiled and minified CSS -->
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-       
+       <!-- <link rel="stylesheet" href="css/styles.css"> -->
+       <link rel="stylesheet" href="{{URL::to('css/styles.css')}}">
     </head>
     <body>
     	@include('partials.header')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,32 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="row">
-    <div class="col-md-12">
-        <h1>Control Struture</h1>
-            @if(true)
-                <p>This is only display if if is true</p>
-            @else
-                <p>This is only display if if is false</p>
-            @endif
-            <hr>
-            @for($i = 0; $i < 5; $i++)
-                <p>{{$i + 1}} Iteration</p>
-            @endfor
-            <h2>XSS</h2>
-            {{ "<script>alert('Hello')</script>"}}
-            {!! "<script>alert('Hello')</script>" !!}
-            
-            
-
+  <div class="row">
+        <div class="col-md-12">
+            <p class="quote">The beautiful Laravel</p>
+        </div>
     </div>
-</div>
-
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">Learning Laravel</h1>
+            <p>This blog post will get you right on track with Laravel!</p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">The next Steps</h1>
+            <p>Understanding the Basics is great, but you need to be able to make the next steps.</p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">Laravel 5.3</h1>
+            <p>Though announced as a "minor release", Laravel 5.3 ships with somer very interesting additions and features.</p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
 @endsection
