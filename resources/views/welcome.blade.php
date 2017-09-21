87860b7b4aba28c7561947001103a68b6ca4bd44
thuytiensp <thuytiensp@gmail.com>
2017-09-21 17:05:22 +0700

    Control Structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,16 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        <h1>Some Content</h1>
-        {{"Hello Tien"}}
+        <h1>Control Struture</h1>
+            @if(true)
+                <p>This is only display if if is true</p>
+            @else
+                <p>This is only display if if is false</p>
+            @endif
+            <hr>
+            @for($i = 0; $i < 5; $i++)
+                <p>{{$i + 1}} Iteration</p>
+            @endfor
     </div>
 </div>
 
