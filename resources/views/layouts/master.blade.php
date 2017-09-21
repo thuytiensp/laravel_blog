8d424acb8c1b2bca35e713c41ad13c3df0a581de
thuytiensp <thuytiensp@gmail.com>
2017-09-21 17:00:19 +0700

    Demo partial

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,9 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>     
+        <!-- Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
        
     </head>
     <body>
-		@yield('content')
+    	@include('partials.header')
+    	<div class="container">
+    		@yield('content')
+    	</div>		
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-<h1>Some Content</h1>
+<div class="row">
+    <div class="col-md-12">
+        <h1>Some Content</h1>
+        {{"Hello Tien"}}
+    </div>
+</div>
+
 @endsection
