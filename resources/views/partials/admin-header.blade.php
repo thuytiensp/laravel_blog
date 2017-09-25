18bc9b85504a774834e81736bd0574ca9c5347b0
thuytiensp <thuytiensp@gmail.com>
2017-09-25 10:34:23 +0700

    add some routes and views for admin, post and other

---
+++ resources/views/partials/admin-header.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Laravel Guide</a>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Posts</a></li>
+            </ul>
+        </div>
+    </div><!-- /.container-fluid -->
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +11,30 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
 });
+
+Route::get('post/{id}', function () {
+    return view('blog.post');
+});
+
+Route::get('about', function () {
+    return view('other.about');
+});
+
+Route::get('admin/create', function () {
+    return view('admin.create');
+});
+
+Route::get('admin', function () {
+    return view('admin.index');
+});
+
+Route::get('admin/edit/{id}', function () {
+    return view('admin.edit');
+});
+
+Route::get('admin/update', function () {
+    return view('admin.update');
+});
+
