d790c3091305f7bcdf333178b6fa9e842a8b8764
thuytiensp <thuytiensp@gmail.com>
2017-09-25 10:46:46 +0700

    Adding Navigation and Passing Route Parameters

--- resources/views/partials/admin-header.blade.php
+++ resources/views/partials/admin-header.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">Laravel Guide</a>
+            <a class="navbar-brand" href="{{route('blog.index')}}">Laravel Guide</a>
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Posts</a></li>
+                <li class="active"><a href="{{route('admin.index')}}">Posts</a></li>
             </ul>
         </div>
     </div><!-- /.container-fluid -->
--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,29 @@
 
 Route::get('/', function () {
     return view('blog.index');
-});
+})->name('blog.index');
 
 Route::get('post/{id}', function () {
     return view('blog.post');
-});
+})->name('blog.post');
 
 Route::get('about', function () {
     return view('other.about');
-});
+})->name('other.about');
 
 Route::get('admin/create', function () {
     return view('admin.create');
-});
+})->name('admin.create');
 
 Route::get('admin', function () {
     return view('admin.index');
-});
+})->name('admin.index');
 
 Route::get('admin/edit/{id}', function () {
     return view('admin.edit');
-});
+})->name('admin.edit');
 
 Route::get('admin/update', function () {
     return view('admin.update');
-});
+})->name('admin.update');
 
