ab7d1813ae6011afd6cd84675412a1cb58551e97
thuytiensp <thuytiensp@gmail.com>
2017-09-25 11:00:49 +0700

    Route group

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,41 @@ Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
 
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', function () {
+        return view('admin.index');
+    })->name('admin.index');
+
+    Route::get('create', function () {
+        return view('admin.create');
+    })->name('admin.create');
+
+    Route::post('create', function() {
+        return "It works!";
+    })->name('admin.create');
+
+    Route::get('edit/{id}', function () {
+        return view('admin.edit');
+    })->name('admin.edit');
+
+    Route::post('edit', function() {
+        return "It works!";
+    })->name('admin.update');
+});
+
+/*
+Route::get('/', function () {
+    return view('blog.index');
+})->name('blog.index');
+
+Route::get('post/{id}', function () {
+    return view('blog.post');
+})->name('blog.post');
+
+Route::get('about', function () {
+    return view('other.about');
+})->name('other.about');
+
 Route::get('admin/create', function () {
     return view('admin.create');
 })->name('admin.create');
@@ -42,3 +77,4 @@ Route::post('admin/update', function () {
     return view('admin.update');
 })->name('admin.update');
 
+*/
