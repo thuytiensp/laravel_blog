f4e47eb613450453288f72625e8f757864b5ee02
thuytiensp <thuytiensp@gmail.com>
2017-10-01 22:24:03 +0700

    Eager loading

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,7 +26,7 @@ class PostController extends Controller
     public function getPost( $id)
     {
         #$post = Post::find($id);
-        $post = Post::where('id', '=', $id)->first();
+        $post = Post::where('id', '=', $id)->with('likes')->first();
         return view('blog.post', ['post' => $post]);
     }
     
