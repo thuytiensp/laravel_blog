6d6194a6ff0aa1d3ee555d47957c22305d953385
thuytiensp <thuytiensp@gmail.com>
2017-10-01 17:46:27 +0700

    Defining one-to-many relationship

--- app/Post.php
+++ app/Post.php
@@ -5,5 +5,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
 	protected $fillable = ['title', 'content']; #same as constructor
+
+	public function likes()
+	{
+		return $this->hasMany('App\Like','post_id');
+	}
 }
 ?>
